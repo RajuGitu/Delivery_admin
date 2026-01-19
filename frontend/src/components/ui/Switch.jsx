b559f5cb55baeecb08883014e102c5f1f64a4955
Simran Yelave <simranyelave064@gmail.com>
2026-01-20 00:20:40 +0530

    raj

--- frontend/src/components/ui/Switch.jsx
+++ frontend/src/components/ui/Switch.jsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from "react"
 import * as SwitchPrimitives from "@radix-ui/react-switch"
 
-import { cn } from "../../libs/utils"
+import { cn } from "../../lib/utils"
 
 const Switch = forwardRef((props, ref) => {
   const { className, ...rest } = props
--- frontend/src/hooks/use-toast.js
+++ frontend/src/hooks/use-toast.js
@@ -1,24 +1,116 @@
 import * as React from "react";
 
-const MOBILE_BREAKPOINT = 768;
+const TOAST_LIMIT = 1;
+const TOAST_REMOVE_DELAY = 1000000;
 
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState(undefined);
+let count = 0;
+
+function genId() {
+  count = (count + 1) % Number.MAX_SAFE_INTEGER;
+  return count.toString();
+}
+
+const toastTimeouts = new Map();
+
+const listeners = [];
+let memoryState = { toasts: [] };
+
+function dispatch(action) {
+  memoryState = reducer(memoryState, action);
+  listeners.forEach((listener) => {
+    listener(memoryState);
+  });
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_TOAST":
+      return {
+        ...state,
+        toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT),
+      };
+
+    case "UPDATE_TOAST":
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === action.toast.id ? { ...t, ...action.toast } : t
+        ),
+      };
+
+    case "DISMISS_TOAST":
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === action.toastId ? { ...t, open: false } : t
+        ),
+      };
+
+    case "REMOVE_TOAST":
+      return {
+        ...state,
+        toasts: state.toasts.filter((t) => t.id !== action.toastId),
+      };
+
+    default:
+      return state;
+  }
+}
+
+function addToRemoveQueue(toastId) {
+  if (toastTimeouts.has(toastId)) {
+    return;
+  }
+
+  const timeout = setTimeout(() => {
+    toastTimeouts.delete(toastId);
+    dispatch({ type: "REMOVE_TOAST", toastId });
+  }, TOAST_REMOVE_DELAY);
+
+  toastTimeouts.set(toastId, timeout);
+}
+
+export function toast({ ...props }) {
+  const id = genId();
+
+  dispatch({
+    type: "ADD_TOAST",
+    toast: {
+      ...props,
+      id,
+      open: true,
+      onOpenChange: (open) => {
+        if (!open) {
+          dispatch({ type: "DISMISS_TOAST", toastId: id });
+        }
+      },
+    },
+  });
+
+  return {
+    id,
+    dismiss: () => dispatch({ type: "DISMISS_TOAST", toastId: id }),
+    update: (props) =>
+      dispatch({
+        type: "UPDATE_TOAST",
+        toast: { ...props, id },
+      }),
+  };
+}
+
+export function useToast() {
+  const [state, setState] = React.useState(memoryState);
 
   React.useEffect(() => {
-    const mql = window.matchMedia(
-      `(max-width: ${MOBILE_BREAKPOINT - 1}px)`
-    );
-
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+    listeners.push(setState);
+    return () => {
+      const index = listeners.indexOf(setState);
+      if (index > -1) listeners.splice(index, 1);
     };
-
-    mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-
-    return () => mql.removeEventListener("change", onChange);
   }, []);
 
-  return !!isMobile;
+  return {
+    ...state,
+    toast,
+  };
 }
